9b3400670d695288251c4722f4800af53e48a36e
Tristão <joao.tristao.ext@dasa.com.br>
2018-05-20 19:01:51 -0300

    routes auth

--- routes.js
+++ routes.js
@@ -1,13 +1,15 @@
 const express = require('express'),
     user = require('./routes/user/routes'),
     vehicle = require('./routes/vehicle/routes'),
-    score = require('./routes/score/routes');
+    score = require('./routes/score/routes'),
+    auth = require('./routes/auth/routes');
 
 const router = express.Router();
 
 router.use('/user', user);
 router.use('/vehicle', vehicle);
 router.use('/score', score);
+router.use('/auth', auth);
 
 module.exports = router;
 
